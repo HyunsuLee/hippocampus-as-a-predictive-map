48d267e9a9ae97ea261c2b20951f347011951754
HyunsuLee <hyunspookey@gmail.com>
2019-11-28 15:54:04 +0900

    SR agent testd 2D maze
    1. figures tested 10 x 10 size maze
    sr matrix and SR probability matrix near the barrier

--- 2D_maze_test.py
+++ 2D_maze_test.py
@@ -10,14 +10,14 @@ import sr
 
 # maze env
 
-MAZE_LENGTH = 10
+MAZE_LENGTH = 100
 
 square_maze = mm.square_maze(MAZE_LENGTH)
 
 # put barrier
-B_LENGTH = 5
-B_X_POSTION = 2
-B_Y_POSTION = 4
+B_LENGTH = 40
+B_X_POSTION = 20
+B_Y_POSTION = 40
 barriers = [[B_Y_POSTION, y] for y in range(B_X_POSTION, B_X_POSTION + B_LENGTH)]
 
 square_with_barrier = mm.make_barrier_maze_square(square_maze, \
@@ -73,4 +73,80 @@ def choose_action(state, policy = False):
         assert False
     return action
 
-print(step([0,0], choose_action([0,0])))
+# run SR model
+
+# prepare for SR matrix
+all_states = [[x, y] for x in range(square_with_barrier.shape[0]) \
+    for y in range(square_with_barrier.shape[1])]
+
+sr_matrix = np.eye(len(all_states), dtype=np.float)
+
+# history_sr_matrix = []
+
+# history_exp_idx = 0
+# 2D maze에서 history를 보는 것은 힘든 것 같다.
+
+for i in tqdm(range(10)):
+    state = START
+    
+    while state != END:
+        action = choose_action(state)
+        next_state, _ = step(state, action)
+        sr_matrix = sr.update_SR_matrix(state, next_state, sr_matrix, \
+             all_states, alpha = alpha, gamma = gamma)
+
+        state = next_state
+
+
+sr_point = copy.deepcopy(sr_matrix[:, 3050])
+    
+ 
+def figure_matrix():  
+    fig, ax = plt.subplots() 
+    ax.imshow(sr_matrix, cmap='gray', interpolation='nearest')
+    '''
+    for idx in range(sr_matrix.shape[0]):
+        for jdx in range(sr_matrix.shape[1]):
+            text = ax.text(jdx, idx,round(sr_matrix[idx, jdx],2), \
+                ha = "center", va = "center", color = "b")
+    '''        
+    fig.tight_layout()
+    plt.savefig('./images/sr_SQUARE_' + str(MAZE_LENGTH) + '.png')
+    plt.close()
+
+def figure_sr_point():
+    fig, ax = plt.subplots() 
+    ax.imshow(sr_point.reshape(square_maze.shape),\
+         cmap='gray', interpolation='nearest')
+    '''
+    for idx in range(sr_matrix.shape[0]):
+        for jdx in range(sr_matrix.shape[1]):
+            text = ax.text(jdx, idx,round(sr_matrix[idx, jdx],2), \
+                ha = "center", va = "center", color = "b")
+    '''        
+    fig.tight_layout()
+    plt.savefig('./images/sr_SQUARE_point' + str(MAZE_LENGTH) + '.png')
+    plt.close()
+
+
+'''
+def figure_history():
+
+    #원래 논문의 figure 2C에 해당됨.
+
+    for_legend = []
+    for idx in range(len(history_sr_matrix)):
+        plt.plot(history_sr_matrix[idx])
+        if idx == 0:
+            for_legend.append("init")
+        else:
+            for_legend.append("10^"+str(idx-1))
+    plt.legend(for_legend, loc = 'upper left')
+    plt.savefig('./images/sr_histroy_' + str(MAZE_LENGTH) + '.png')
+'''
+
+if __name__=='__main__':
+    figure_matrix()
+    figure_sr_point()
+ #   figure_history()
+
